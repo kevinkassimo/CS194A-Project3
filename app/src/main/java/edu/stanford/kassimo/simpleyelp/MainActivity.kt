98420b348f0aa140ae784911f9af84b34c5d6590
Kevin (Kun) Kassimo Qian <kevinkassimo@gmail.com>
2020-10-28 16:00:20 -0700

    first commit

---
+++ app/src/main/java/edu/stanford/kassimo/simpleyelp/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.stanford.kassimo.simpleyelp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY = "j8Clex_l2jutA6RNPmnveDgMWv0ALW30936c81COj-r6LzpdI2bU0EVf8WE0t1u6FwpB8mkssR0LxnSUpnesp-pOPhyaMuJjDfhqfq45SdXRZCe_1WI1THfXJQmZX3Yx"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val restaurants = mutableListOf<YelpRestaurant>()
+        val adapter = RestaurantAdapter(this, restaurants)
+        rvRestaurants.adapter = adapter
+        rvRestaurants.layoutManager = LinearLayoutManager(this)
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        val yelpService = retrofit.create(YelpService::class.java)
+
+        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object: Callback<YelpSearchResult> {
+            override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
+                Log.i(TAG, "onResponse $response")
+                val body = response.body()
+                if (body == null) {
+                    Log.w(TAG, "Did not receive valid response body from Yelp API... exiting")
+                    return
+                }
+                restaurants.addAll(body.restaurants)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+        })
+    }
+}
---
+++ app/src/main/java/edu/stanford/kassimo/simpleyelp/YelpService.kt
@@ -0,0 +1,14 @@
+package edu.stanford.kassimo.simpleyelp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+public interface YelpService {
+    @GET("businesses/search")
+    fun searchRestaurants(
+        @Header("Authorization") authHeader: String,
+        @Query("term") searchTerm: String,
+        @Query("location") location: String) : Call<YelpSearchResult>
+}
